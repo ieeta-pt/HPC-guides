3eb5131586a338eb19c1054fb6000c35fa581b44
Tiago Almeida <tiagomeloalmeida@ua.pt>
2024-07-05 15:27:04 +0100

    Create lunch_cuda.sh

---
+++ examples/cuda/lunch_cuda.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=Cuda                   # create a short name for your job
+#SBATCH --output="Cuda-%j.out" # %j will be replaced by the slurm jobID
+#SBATCH --nodes=1                         # node count
+#SBATCH --ntasks=1                        # total number of tasks across all nodes
+#SBATCH --cpus-per-task=2                 # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --gres=gpu:1                      # number of gpus per node
+
+# run the compiled binary
+./vector_addition
