bb675e71798cfc94ed9d466ccc35c79226cc609d
Tiago Almeida <tiagomeloalmeida@ua.pt>
2024-11-07 13:44:21 +0000

    Update hf_trainer.sh

--- examples/transformers/hf_trainer.sh
+++ examples/transformers/hf_trainer.sh
@@ -3,8 +3,9 @@
 #SBATCH --output="hf_trainer-%j.out" # %j will be replaced by the slurm jobID
 #SBATCH --nodes=1                         # node count
 #SBATCH --ntasks=1                        # total number of tasks across all nodes
-#SBATCH --cpus-per-task=4                 # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --cpus-per-task=2                 # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --gres=gpu:nvidia-rtx-a6000:1                      # number of gpus per node
+#SBATCH --partition=gpu                   # Partition where job will be submitted
 #SBATCH --mem-per-cpu=2G
 source virtual-venv/bin/activate
 
